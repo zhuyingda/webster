c03769212201196c96cf956eea6702b03a18f7c7
zhuyingda <zhuyingda@baidu.com>
2018-07-05 15:24:34 +0800

    单元测试用例 含produce consume

--- test/webster.test.js
+++ test/webster.test.js
@@ -29,10 +29,68 @@ describe('webster unit test', function() {
         it('should not two task id equal', function() {
             assert.notEqual(task1.id, task2.id);
         });
-        
+
         it('should two task url equal', function () {
             assert.equal(task1.url, task2.url);
         });
     });
 
+    describe('task module consumer & producer', function() {
+        this.timeout(15000);
+        it('test producer', function (done) {
+            const Producer = require('../lib/producer');
+            let myProducer = new Producer({
+                channel: 'test',
+                dbConf: {
+                    redis: {
+                        host: 'redis-13818.c8.us-east-1-2.ec2.cloud.redislabs.com',
+                        port: 13818,
+                        password: 'n0JZ9pBM0oLHLITjyyHRiRAPAm2k4ftN'
+                    }
+                }
+            });
+            const Task = require('../lib/task');
+            let task = new Task({
+                spiderType: 'browser',
+                url: 'https://www.baidu.com/s?wd=javascript',
+                targets: [
+                    {
+                        selector: '.result.c-container h3',
+                        type: 'text',
+                        field: 'title'
+                    }
+                ]
+            })
+            myProducer.generateTask(task).then(() => {
+                done();
+            });
+        });
+        it('test consumer for plain crawling', function (done) {
+            const Consumer = require('../lib/consumer');
+            class TestConsumer extends Consumer {
+                constructor(option) {
+                    super(option);
+                }
+                afterCrawlRequest(result) {
+                    console.log(result);
+                    done();
+                    process.exit();
+                }
+            }
+            let myConsumer = new TestConsumer({
+                channel: 'test',
+                sleepTime: 5000,
+                deviceType: 'pc',
+                userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+                dbConf: {
+                    redis: {
+                        host: 'redis-13818.c8.us-east-1-2.ec2.cloud.redislabs.com',
+                        port: 13818,
+                        password: 'n0JZ9pBM0oLHLITjyyHRiRAPAm2k4ftN'
+                    }
+                }
+            });
+            myConsumer.startConsume();
+        });
+    });
 });
