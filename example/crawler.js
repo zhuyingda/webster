07f616e96d4d64d2b189c60ae68a341b2d6c6d1c
赫焱 <yingda.zyd@antgroup.com>
2023-11-09 18:33:31 +0800

    docker base-image switch from centos to ubuntu

--- example/crawler.js
+++ example/crawler.js
@@ -1,10 +1,14 @@
+/**
+ * usage outside docker:
+ * docker run --tty -e URL="https://www.tiktok.com/" -e Cookie="foo=1234; bar=abcd" -e UA="Mozilla/115.0 AppleWebKit/537.36 Chrome/116" imageId node crawler.js
+ * usage inside docker:
+ * env URL="https://www.tiktok.com/" Cookie="foo=1234; bar=abcd" UA="Mozilla/115.0 AppleWebKit/537.36 Chrome/116" node crawler.js
+ */
 const { spider } = require('webster');
-
-let _ua = '';
 
 class MySpider extends spider {
     get defUserAgent() {
-        return _ua;
+        return `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36`;
     }
     get defDeviceType() {
         return 'pc';
@@ -15,6 +19,13 @@ class MySpider extends spider {
 }
 
 (async () => {
+    const url = process.env.URL || `https://www.tiktok.com/`;
+    const cookie = process.env.Cookie || ``;
+    const ua = process.env.UA || `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36`;
+    if (process.env.debug) {
+        console.log('crawl input:', url, cookie, ua);
+    }
+
     const spider = new MySpider({
         type: 'browser',
         engine: 'playwright',
@@ -31,17 +42,25 @@ class MySpider extends spider {
                 field: 'result'
             }
         ],
+        customHeaders: {
+            'Cookie': cookie,
+            'User-Agent': ua,
+        }
     });
-    const url = process.env.URL || `https://www.zhuyingda.com/note`;
-    const cookie = process.env.Cookie || ``;
-    _ua = process.env.UA || `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36`;
-    console.log('crawl input:', url, cookie, _ua);
+
+    if (process.env.debug) {
+        console.log('crawling start');
+    }
     let crawlResult = await spider.startRequest(url);
+    if (process.env.debug) {
+        console.log('crawling end');
+    }
     if (crawlResult.result) {
         console.log(crawlResult.result[0].text);
+        process.exit(0);
     }
     else {
-        console.error('webster error:', crawlResult);
+        console.error('crawling error:', crawlResult);
+        process.exit(1);
     }
-
 })();
